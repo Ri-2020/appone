b1540538aac6f75f57a4e6ebbef4d592f7fea4df
Ri-2020 <rohitgupta111abcd@gmail.com>
2022-08-29 16:35:52 +0530

    some changes

--- app.js
+++ app.js
@@ -22,15 +22,8 @@ const data = [{
 },
 ]
 
-app.get('/', (req, res , next) => {
-  res.send('Hello World!');
-  next();
-})
-
-app.use('/' , (req, res, next) => {
-  console.log("suecess");
-});
 app.get('/:name', (req, res) => {
+  // res.send(`${name}`);
   const requireddata = data.filter((item) => item.name === req.params.name);
 
   requireddata.length !=0 ? res.send(`${requireddata[0].stars}`) : res.send('No data found');
@@ -38,6 +31,13 @@ app.get('/:name', (req, res) => {
   // we need to send the string in the response
   // if we send the number as response it takse the numer as a status code
 })
+
+
+app.get('/', (req, res , next) => {
+  res.send('Hello!');
+  next();
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
